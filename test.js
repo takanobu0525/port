db7bac40d1e0b25e16d7d1f38cf959ef2a298904
takanobu0525 <66146535+takanobu0525@users.noreply.github.com>
2020-06-10 00:07:17 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,27 @@
+$(function(){
+	setTimeout(function(){
+		$('.top h1').slideDown(1500);
+	},2500);
+	setTimeout(function(){
+		$('.top p').fadeIn(1500);
+	},1000);
+	setTimeout(function(){
+		$('body').fadeIn(1000);
+	},0);
+	
+$(".btnlist").on("click", function() {
+	const scrollTarget = $(this)[0].attributes[1].nodeValue;
+	const offsetTop = $(scrollTarget).offset().top;
+	$("html, body").animate({ scrollTop: offsetTop }, 500);
+	return false;
+});
+	$(function() {
+		$('header').click(function() {
+			$('.header-right').fadeIn();
+		});
+		$('.close-modal').click(function() {
+			$('#login-modal').fadeOut();
+			$('#signup-modal').fadeOut();
+		});
+	});
+});
